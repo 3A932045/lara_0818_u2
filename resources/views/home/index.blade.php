d25531808e919cbc905195405aaa57c79d7c92cd
3A932045 <shadowy999@gmail.com>
2022-11-05 00:52:33 +0800

    建立子樣板

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,17 @@
+<!--<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首頁</title>
+</head>
+<body>
+<p>test html</p>
+</body>
+</html>
+-->
+@extends('layouts.master')
+@section('title','Home')
+@section('content')
+    <h1>Home</h1>
+    <p>This is my body content.</p>
+@endsection
